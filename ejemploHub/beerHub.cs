68422dda9d23cfe28e2ccfbb09410ee98336c79f
RodrigoSanchezPeregr <RodrigoSanchezPeregr@LTLPMXRSANCHEZ>
2023-09-28 17:32:07 -0600

    Agregar archivos de proyecto.

---
+++ ejemploHub/beerHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ejemploHub
+{
+    public class beerHub:Hub
+    {
+        public async Task send(string name,string brand)
+        {
+            await Clients.All.SendAsync("recibir", name, brand);
+        }
+    }
+}
